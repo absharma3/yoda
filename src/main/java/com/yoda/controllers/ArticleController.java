4ca6d498be888f6a4bfb3fb5ea776e8a2eccb540
absharma3 <master.abhimanyu@gmail.com>
2018-03-06 19:42:08 +0530

    Added get requests for articles

--- src/main/java/com/yoda/controllers/ArticleController.java
+++ src/main/java/com/yoda/controllers/ArticleController.java
@@ -5,10 +5,9 @@ import com.yoda.repository.ArticleRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 /**
  * Created by abhimanyus on 2/27/18.
@@ -24,6 +23,16 @@ public class ArticleController {
     @RequestMapping(path = "", method = RequestMethod.POST, produces = {MediaType.APPLICATION_JSON_VALUE})
     public @ResponseBody Article addArticle(@RequestBody Article article) {
         return articleRepository.save(article);
+    }
+
+    @RequestMapping(path = "", method = RequestMethod.GET, produces = {MediaType.APPLICATION_JSON_VALUE})
+    public @ResponseBody List<Article> articles() {
+        return articleRepository.findAll();
+    }
+
+    @RequestMapping(path = "/{articleId}", method = RequestMethod.GET, produces = {MediaType.APPLICATION_JSON_VALUE})
+    public @ResponseBody Article article(@PathVariable String articleId) {
+        return articleRepository.findOne(articleId);
     }
 
 
